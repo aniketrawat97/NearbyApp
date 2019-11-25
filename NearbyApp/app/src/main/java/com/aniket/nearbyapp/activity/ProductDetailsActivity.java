dd5c0674917e5cd0f807b67386e0e13df9b31d71
Golu-Vijay1997 <dvijay11nov@1997gmail.com>
2019-11-25 12:44:02 +0530

    Modified java files with variables

--- NearbyApp/app/src/main/java/com/aniket/nearbyapp/activity/ProductDetailsActivity.java
+++ NearbyApp/app/src/main/java/com/aniket/nearbyapp/activity/ProductDetailsActivity.java
@@ -3,14 +3,35 @@ package com.aniket.nearbyapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.aniket.nearbyapp.R;
+import com.aniket.nearbyapp.models.Store;
 
 public class ProductDetailsActivity extends AppCompatActivity {
-
+    Store store1;
+    TextView finalPrice,beforePrice,Store_name,Prod_title,description1,description2;
+    ImageView Image_prod;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+        Store_name=findViewById(R.id.TV_Prod_details);
+        Store_name.setText(store1.name);
+        Image_prod=findViewById(R.id.IV_Prod_details);
+        Image_prod.setImageBitmap(store1.cards.get(0).image);
+        Prod_title=findViewById(R.id.TV_Prod_details_title);
+        Prod_title.setText(store1.cards.get(0).title);
+        finalPrice=findViewById(R.id.TV_Prod_details_final_price);
+        finalPrice.setText(store1.cards.get(0).finalPrice);
+        beforePrice=findViewById(R.id.TV_Prod_details_before_price);
+        beforePrice.setText( store1.cards.get(0).beforePrice);
+        description1=findViewById(R.id.TV_Prod_details_description1);
+        description1.setText(store1.cards.get(0).desc1);
+        description2=findViewById(R.id.TV_Prod_details_description2);
+        description2.setText(store1.cards.get(0).desc2);
+
+
     }
 }
--- NearbyApp/app/src/main/java/com/aniket/nearbyapp/activity/StoreDetailsActivity.java
+++ NearbyApp/app/src/main/java/com/aniket/nearbyapp/activity/StoreDetailsActivity.java
@@ -3,14 +3,36 @@ package com.aniket.nearbyapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.aniket.nearbyapp.R;
+import com.aniket.nearbyapp.models.Store;
 
 public class StoreDetailsActivity extends AppCompatActivity {
-
+    EditText cardTitle,finalPrice,beforePrice,description1,description2;
+    TextView Upload;
+    String cardTitle_TC,finalPrice_TC,beforePrice_TC,description1_TC,description2_TC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_details);
+        cardTitle=findViewById(R.id.ET_Store_details_card_title);
+        beforePrice=findViewById(R.id.ET_Store_details_Before_price);
+        finalPrice=findViewById(R.id.ET_Store_details_Final_price);
+        description1=findViewById(R.id.ET_Store_details_description1);
+        description2=findViewById(R.id.ET_Store_details_description2);
+        Upload=findViewById(R.id.TV_Store_details_upload);
+        Upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cardTitle_TC=cardTitle.getText().toString();
+                beforePrice_TC=beforePrice.getText().toString();
+                finalPrice_TC=finalPrice.getText().toString();
+                description1_TC=description1.getText().toString();
+                description2_TC=description2.getText().toString();
+            }
+        });
     }
 }
